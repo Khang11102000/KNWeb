420828c99326561252d837b540efc5cb96937bbf
namnguyen <nguyenkimquocnam.dev@gmail.com>
2024-09-17 13:55:00 +0700

    feat: add loading UI

---
+++ client/src/app/loading.tsx
@@ -0,0 +1,9 @@
+import Loading from '@/components/shared/loading'
+import { Spin } from 'antd'
+import React from 'react'
+
+const LoadingPage = () => {
+  return <Loading />
+}
+
+export default LoadingPage
