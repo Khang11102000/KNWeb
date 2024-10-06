70a21a23629a0efca4bef78e32fb04070eaa5617
Nam Nguyễn <namnkq2603@gmail.com>
2024-10-06 10:47:09 +0700

    feat: display all friends on home page

--- client/src/app/(user)/_components/user-card/index.tsx
+++ client/src/app/(user)/_components/user-card/index.tsx
@@ -1,17 +1,26 @@
+'use client'
 import { IUser } from '@/types/user-type'
 import { Avatar, Button } from 'antd'
 import React from 'react'
 import styles from './user-card.module.scss'
 import clsx from 'clsx'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 const { userCard, userInfo, userName } = styles
 
 interface IUserCardProps {
   user: IUser
+  isAddFriend?: boolean
 }
 
-const UserCard = ({ user }: IUserCardProps) => {
+const UserCard = ({ user, isAddFriend }: IUserCardProps) => {
   const { firstName, lastName, photo, email } = user || {}
+  const router = useRouter()
+
+  const handleNavigateToChat = () => {
+    router.push('/message')
+  }
 
   return (
     <div className={clsx(userCard)}>
@@ -20,12 +29,18 @@ const UserCard = ({ user }: IUserCardProps) => {
         <Avatar size={48} src={photo} />
         <div>
           <p className={clsx(userName)}>{`${firstName} ${lastName}`}</p>
-          {email && <p>@{`${email}`}</p>}
+          {/* {email && <p>@{`${email}`}</p>} */}
         </div>
       </div>
 
       {/* Right */}
-      {/* <Button type='primary'>Add Friend</Button> */}
+      {isAddFriend ? (
+        <Button type='primary'>Add Friend</Button>
+      ) : (
+        <Button type='primary' onClick={() => handleNavigateToChat()}>
+          Chat
+        </Button>
+      )}
     </div>
   )
 }
